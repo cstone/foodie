0b13053092583d59baa3f203927bd0f888c451e1
Cavin Stone <cstone2003@hotmail.com>
2013-11-14 22:14:18 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodie::Application.config.secret_token = '536fd19b717e117a38a2aa4c386fe1d73e33ae6c619303e6adc48a455778744f19a28481618d04fa9fe8902fe3c5b76c5d755607bd701d96d610b48cac765c75'
